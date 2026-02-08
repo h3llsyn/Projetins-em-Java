7b822a4cdf77de0003996edbb9c6d17f34ccbb75
Lavínia <lavizsenai@gmail.com>
2026-02-08 15:27:17 -0300

    dando erro em tudo eba

--- Cantina JavaFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.cantinajavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.cantinajavafx to javafx.fxml;
-    exports com.example.cantinajavafx;
-}
