106d8f4f14a05ca51e3008c66b7b0a4b600091f5
Lavínia <lavizsenai@gmail.com>
2026-02-08 20:00:10 -0300

    ta indo

--- Cadastro-de-Usuarios/src/main/java/com/example/cadastrodeusuarios/HelloController.java
+++ Cadastro-de-Usuarios/src/main/java/com/example/cadastrodeusuarios/HelloController.java
@@ -2,13 +2,32 @@ package com.example.cadastrodeusuarios;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField input;
+    @FXML
+    private ListView<String> listViewUsuarios;
+    @FXML
+    private AnchorPane container;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        container.requestFocus();
+    }
+
+    @FXML
+    private void cadastrarUsuario() {
+        String nomeUsuario = input.getText();
+        if (nomeUsuario.length() >= 2 || listViewUsuarios.contains(nomeUsuario)) {
+            listViewUsuarios.getItems().add(nomeUsuario);
+        }
     }
 }
+
+//input
+//cadastrarButton
+//listViewUsuarios
