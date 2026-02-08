ddbdfc6fc26d4376a9d92624569ff39d879c1ca7
Lavínia <lavizsenai@gmail.com>
2026-02-08 16:22:04 -0300

    arrumei, começando o cadastro de ususarios

--- Cadastro-de-Usuarios/src/main/java/com/example/cadastrodeusuarios/HelloApplication.java
+++ Cadastro-de-Usuarios/src/main/java/com/example/cadastrodeusuarios/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 820, 490);
+        stage.setTitle("Cadastro de Usuários");
         stage.setScene(scene);
         stage.show();
     }
